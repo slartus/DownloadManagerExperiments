7ab02275ecc10e075f90b104011684674d09396e
slartus <slartus@gmail.com>
2021-12-06 12:33:05 +0500

    check permissions if needed

--- app/src/main/java/ru/slartus/downloadmanagerexperiments/MainActivity.kt
+++ app/src/main/java/ru/slartus/downloadmanagerexperiments/MainActivity.kt
@@ -1,18 +1,41 @@
 package ru.slartus.downloadmanagerexperiments
 
+import android.Manifest
 import android.app.DownloadManager
-import android.app.IntentService
+
 import android.content.Context
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import java.lang.StringBuilder
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                download()
+            } else {
+                Toast.makeText(this, "Permission is not granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        when {
+            Build.VERSION.SDK_INT < 23 -> download()
+            Build.VERSION.SDK_INT < 30 -> activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            else -> download()
+        }
+    }
+
+    private fun download() {
         systemDownload(
             this,
             "updateinfo.json",
